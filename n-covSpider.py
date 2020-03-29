e5561ac0c3151dcc4b5e07df9baf67cc0c453021
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-03-29 17:45:29 +0800

    Update n-covSpider.py

--- n-covSpider.py
+++ n-covSpider.py
@@ -2,38 +2,55 @@ import requests
 import json
 from bs4 import BeautifulSoup
 
-def getInformation():
+def getInformationChina():
 	url = "https://view.inews.qq.com/g2/getOnsInfo?name=disease_h5"
 	try:
-		send_headers = {
-		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
-		"Connection": "keep-alive",
-		"Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-		"Accept-Language": "zh-CN,zh;q=0.8"
-		}#伪装成浏览器
-
-		r = requests.get(url,send_headers)
+		r = requests.get(url)
 		r.raise_for_status()
 		r.encoding = r.apparent_encoding
 		return r.text
 	except:
-		return "Error"
+		print("!中国疫情数据抓取错误!")
 
-def dataMake(dataJs, getType):
-	dataLs = json.loads(dataJs)
+def getInformationForeign():
+	url = "https://api.inews.qq.com/newsqa/v1/automation/foreign/country/ranklist"
+	try:
+		r = requests.get(url)
+		r.raise_for_status()
+		r.encoding = r.apparent_encoding
+		return r.text
+	except:
+		print("!国际疫情数据抓取错误!")
+
+def dataMake(dataJs,dataLs):
+	dataLs[0] = json.loads(dataJs[0])
+	dataLs[1] = json.loads(dataJs[1])
 	return dataLs
 
-def main():
-	print("#正在获取数据...\n")
-	dataJs = getInformation()
+def writeFile(place,data):
+    data = str(data)
+    filehandle = open(place,'w', encoding="utf-8")
+    filehandle.write(data)
+    filehandle.close()
 
-	if dataJs == "Error":
+def main():
+	# 疫情分类，分为国内：0，国外：1
+	dataJs = ["none"]*2
+	dataLs = ["none"]*2
+
+	print("#正在获取数据...\n")
+	dataJs[0] = getInformationChina()
+	dataJs[1] = getInformationForeign()
+
+	if dataJs[0] == "none" or dataJs[1] == "none":
 		print("!抓取错误！\n")
 
 	else:
 		print("#获取成功\n")
 		getType = input("#请输入你需要的数据代码\n>>>")
-		result = dataMake(dataJs, getType)
+		result = dataMake(dataJs,dataLs)
 		print(result)
+		writeFile("test.txt",result[0])
+	input("回车退出")
 
 main()
