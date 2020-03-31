fef28f9076dc2d3c563ad771e78e28986cbff7ca
PotatoMani <52829594+PotatoMani@users.noreply.github.com>
2020-03-31 14:47:21 +0800

    Update n-covSpider.py

--- n-covSpider.py
+++ n-covSpider.py
@@ -43,6 +43,94 @@ def MoreDictGet(dict,objkey,default):
 
 	return default			
 
+# 疫情总览处理
+def MainVirous(dataLs):
+	ls = []
+
+	# 中国疫情总览处理+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	data = dataLs[0]
+	# 修补腾讯API格式错误
+	data = eval(data["data"].replace("true","True").replace("false","False"))
+	
+	# 搜索日期数据
+	ls.append("数据更新时间：" + MoreDictGet(data,"lastUpdateTime","none"))
+	
+	ls.append("中国疫情总览------------------------------------------------------")
+	dic1 = MoreDictGet(data,"chinaTotal","none")
+	dic2 = MoreDictGet(data,"chinaAdd","none")
+
+	# 确诊
+	ls.append("确诊:" + "       累计:" + str(dic1["confirm"]) + 
+		"例(新增:" + str(dic2["confirm"]) + ")" + 
+		"     现有:" + str(dic1["nowConfirm"]) + 
+		"例(新增:" + str(dic2["nowConfirm"]) + ")")
+
+	# 治愈
+	ls.append("治愈:" + "       累计:" + str(dic1["heal"]) + 
+		"例(新增:" + str(dic2["heal"]) + ")")
+
+	# 死亡
+	ls.append("死亡:" + "       累计:" + str(dic1["dead"]) + 
+		"例(新增:" + str(dic2["dead"]) + ")")
+
+	# 疑似
+	ls.append("疑似:" + "       现有:" + str(dic1["suspect"]) + 
+		"例(新增:" + str(dic2["suspect"]) + ")")
+
+	# 境外输入
+	ls.append("境外输入:" + "   累计:" + str(dic1["importedCase"]) + 
+		"例(新增:" + str(dic2["importedCase"]) + ")")
+	
+	# 重症
+	ls.append("重症:" + "       现有:" + str(dic1["nowSevere"]) + 
+		"例(新增:" + str(dic2["nowSevere"]) + ")")
+
+	# 国际疫情总览处理+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+	data = dataLs[1]
+	dic = MoreDictGet(data,"data","none")
+
+	wdConfirmAdd = 0
+	wdNowConfirm = 0
+	wdConfirm = 0
+	wdDead = 0
+	wdHeal = 0
+	wdNowConfirmCompare = 0
+	wdHealCompare = 0
+	wdDeadCompare = 0
+
+	for di in dic:
+		# 全部数据计算
+		wdConfirmAdd = MoreDictGet(di,"confirmAdd","none") + wdConfirmAdd
+		wdNowConfirm = MoreDictGet(di,"nowConfirm","none") + wdNowConfirm
+		wdConfirm = MoreDictGet(di,"confirm","none") + wdConfirm
+		wdDead = MoreDictGet(di,"dead","none") + wdDead
+		wdHeal = MoreDictGet(di,"heal","none") + wdHeal
+		wdNowConfirmCompare = MoreDictGet(di,"nowConfirmCompare","none") + wdNowConfirmCompare
+		wdHealCompare = MoreDictGet(di,"healCompare","none") + wdHealCompare
+		wdDeadCompare = MoreDictGet(di,"deadCompare","none") + wdDeadCompare
+
+
+	ls.append("\n世界疫情总览-------------------------------------------------------")
+
+	# 确诊
+	ls.append("确诊:" + "       累计:" + str(wdConfirm) + 
+		"例(新增:" + str(wdConfirmAdd) + ")" + 
+		"     现有:" + str(wdNowConfirm) + 
+		"例(新增:" + str(wdNowConfirmCompare) + ")")
+
+	# 治愈
+	ls.append("治愈:" + "       累计:" + str(wdHeal) + 
+		"例(新增:" + str(wdHealCompare) + ")")
+
+	# 死亡
+	ls.append("死亡:" + "       累计:" + str(wdDead) + 
+		"例(新增:" + str(wdDeadCompare) + ")")
+
+	return ls
+
+# 中国疫情处理
 def chinsVirous(data):
 	ls = []
 	# 修补腾讯API格式错误
@@ -84,7 +172,7 @@ def chinsVirous(data):
 	ls.append("中国各省疫情------------------------------------------------")
 
 	dic = MoreDictGet(data,"areaTree","none")
-	dic = MoreDictGet(dic[0],"children","none")
+	dic = MoreDictGet(dic[0],"children","none")# 解决列表问题
 
 	ls.append("编号 省份  现有确诊  现有疑似  累计确诊  治愈  死亡")
 	num = 0
@@ -107,6 +195,11 @@ def chinsVirous(data):
 
 	return ls
 
+# 国际疫情处理
+def foreignVirous(data):
+	pass
+
+
 def writeFile(place,data):
     data = str(data)
     file = open(place,'w', encoding="utf-8")
@@ -126,13 +219,19 @@ def main():
 		print("!抓取错误！\n")
 
 	else:
-		print("#获取成功\n")
+		print("#获取成功\n") 
 
 		dataLs = dataMake(dataJs,dataLs)
 
-		chinaRuselt = chinsVirous(dataLs[0])
+		check = input("请输入你要查看的数据类型\n总览：M\n中国疫情：C\n国际疫情：F\n>>>")
+		if check == "M" or check == "m":
+			result = MainVirous(dataLs)
 
-		for i in chinaRuselt:
+		elif check == "C" or check == "c":
+			result = chinsVirous(dataLs[0])
+
+
+		for i in result:
 			print(i + "\n")
 
 		# writeFile("test.txt",result[0])
